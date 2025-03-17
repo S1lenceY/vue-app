630554130102fb99ebded8ec645995719a44fc8e
S1lenceY <u21216338@utp.edu.pe>
2025-03-17 17:44:31 -0500

    Updated Dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,10 +1,9 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head, Link, usePage } from '@inertiajs/vue3';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Head, Link, usePage } from "@inertiajs/vue3";
 
 const page = usePage();
 const cantidadSinProgramar = page.props.cantidadSinProgramar;
-
 </script>
 
 <template>
@@ -12,27 +11,41 @@ const cantidadSinProgramar = page.props.cantidadSinProgramar;
 
     <AuthenticatedLayout>
         <template #header>
-            <h2
-                class="text-xl font-semibold leading-tight text-gray-800"
-            >
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
                 Bienvenido
             </h2>
         </template>
 
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div
-                    class="overflow-hidden shadow-sm sm:rounded-lg"
-                >
+                <div class="overflow-hidden sm:rounded-lg">
                     <div class="py-6 text-green-600 font-bold">
-                        Total de Avisos: {{ cantidadSinProgramar }}
+                        Cantidad Total: {{ cantidadSinProgramar }}
                     </div>
-                    <Link :href="route('sinprogramar')" class="h-20 inline-flex justify-between rounded-xl overflow-hidden bg-yellow-300">
-                        <p class="px-6 text-slate-900 text-base font-normal leading-normal self-center">
-                            Avisos sin programación
-                        </p>
-                        <div class="bg-yellow-200 w-20 flex-shrink-0 flex justify-center items-center text-center text-slate-900 text-2xl font-semibold leading-loose">
-                            {{ cantidadSinProgramar }}
+                    <Link
+                        :href="route('sinprogramar')"
+                        class="h-fit inline-flex"
+                    >
+                        <div
+                            class="hover:-translate-y-2 group bg-neutral-50 duration-500 w-44 h-44 flex text-neutral-600 flex-col justify-center items-center relative rounded-xl overflow-hidden shadow-md"
+                        >
+                            <svg
+                                viewBox="0 0 200 200"
+                                xmlns="http://www.w3.org/2000/svg"
+                                class="absolute blur z-10 fill-green-300 duration-500 group-hover:blur-none group-hover:scale-105"
+                            >
+                                <path
+                                    transform="translate(100 100)"
+                                    d="M39.5,-49.6C54.8,-43.2,73.2,-36.5,78.2,-24.6C83.2,-12.7,74.8,4.4,69,22.5C63.3,40.6,60.2,59.6,49.1,64.8C38.1,70,19,61.5,0.6,60.7C-17.9,59.9,-35.9,67,-47.2,61.9C-58.6,56.7,-63.4,39.5,-70,22.1C-76.6,4.7,-84.9,-12.8,-81.9,-28.1C-79,-43.3,-64.6,-56.3,-49.1,-62.5C-33.6,-68.8,-16.8,-68.3,-2.3,-65.1C12.1,-61.9,24.2,-55.9,39.5,-49.6Z"
+                                ></path>
+                            </svg>
+
+                            <div
+                                class="z-20 flex flex-col justify-center items-center"
+                            >
+                                <span class="font-bold text-6xl ml-2">{{ cantidadSinProgramar }}</span>
+                                <p class="font-bold">Sin Programar</p>
+                            </div>
                         </div>
                     </Link>
                 </div>
